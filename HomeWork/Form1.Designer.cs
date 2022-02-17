c6d04a1d114f8619b71b9a2d759d2dce8ad31ca8
deepa <deepa@DESKTOP-J9VGJIH>
2022-02-17 16:12:16 -0500

    Add project files.

---
+++ HomeWork/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace HomeWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxStudentID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxFirst_Name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxLast_Name = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonValidate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student ID";
+            // 
+            // textBoxStudentID
+            // 
+            this.textBoxStudentID.Location = new System.Drawing.Point(53, 73);
+            this.textBoxStudentID.Name = "textBoxStudentID";
+            this.textBoxStudentID.Size = new System.Drawing.Size(100, 22);
+            this.textBoxStudentID.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "First Name";
+            // 
+            // textBoxFirst_Name
+            // 
+            this.textBoxFirst_Name.Location = new System.Drawing.Point(53, 226);
+            this.textBoxFirst_Name.Name = "textBoxFirst_Name";
+            this.textBoxFirst_Name.Size = new System.Drawing.Size(100, 22);
+            this.textBoxFirst_Name.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 297);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Last Name";
+            // 
+            // textBoxLast_Name
+            // 
+            this.textBoxLast_Name.Location = new System.Drawing.Point(53, 334);
+            this.textBoxLast_Name.Name = "textBoxLast_Name";
+            this.textBoxLast_Name.Size = new System.Drawing.Size(100, 22);
+            this.textBoxLast_Name.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "(7-Digit Number)";
+            // 
+            // buttonValidate
+            // 
+            this.buttonValidate.Location = new System.Drawing.Point(261, 181);
+            this.buttonValidate.Name = "buttonValidate";
+            this.buttonValidate.Size = new System.Drawing.Size(129, 43);
+            this.buttonValidate.TabIndex = 7;
+            this.buttonValidate.Text = "Validate";
+            this.buttonValidate.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonValidate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxLast_Name);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxFirst_Name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxStudentID);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxStudentID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxFirst_Name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxLast_Name;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonValidate;
+    }
+}
+
